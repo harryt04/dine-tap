bf56d80630fbc6a5763e3ebee176e5169b95abbd
Harry Thomas <hsthomas@verisk.com>
2024-06-22 09:07:32 -0600

    restore sign up button

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { DarkModeSwitch } from './darkModeSwitch'
+import SignUpButton from './signUpButton'
 
 export const Footer = () => {
   return (
     <div className="footer-container">
+      <SignUpButton />
       <DarkModeSwitch />
     </div>
   )
---
+++ src/app/components/signUpButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const SignUpButton = () => {
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      href="https://docs.google.com/forms/d/e/1FAIpQLSdbnQ2rEdaF77scxMddKa48wENzeVMGMBXGxiiy__U0ztAqqA/viewform"
+      target="_blank"
+    >
+      SignUp For DineTap
+    </Button>
+  )
+}
+
+export default SignUpButton
