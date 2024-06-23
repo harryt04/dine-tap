93ae8b39f2de88f26cc2bbc6eeff5b7d2e61bd78
Harry Thomas <hsthomas@verisk.com>
2024-06-23 11:32:39 -0600

    prettify

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 
 export const Footer = () => {
   return (
-    <Box component="footer" display="flex" justifyContent="space-between" alignItems="center" p={2} bgcolor="background.paper">
+    <Box
+      component="footer"
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      p={2}
+      bgcolor="background.paper"
+    >
       <Typography variant="caption" color="textSecondary" align="center">
         DineTap &copy; 2024
       </Typography>
     </Box>
-  );
-};
+  )
+}
