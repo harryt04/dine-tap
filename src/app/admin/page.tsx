7d2e172b36a18316578efc41a86706e9521e8242
Harry Thomas <hsthomas@verisk.com>
2024-06-23 16:17:16 -0600

    public facing temp admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,31 @@
+'use client'
 import React from 'react'
+import Header from '../components/header'
+import { Typography } from '@mui/material'
+import SignUpButton from '../components/signUpButton'
+import { useAuth } from '../context/AuthContext'
 
 const Admin = () => {
-  return <div>Admin</div>
+  const { userLoading } = useAuth()
+
+  if (userLoading) return null
+  return (
+    <>
+      <Header />
+
+      <div className="spacer"></div>
+      <div className="spacer"></div>
+
+      <div className="center">
+        <Typography variant="h5">
+          If you are a restaurant owner and would like to subscribe, please do
+          so here.
+        </Typography>
+        <div className="spacer"></div>
+        <SignUpButton label="Join Waitlist" />
+      </div>
+    </>
+  )
 }
 
 export default Admin
